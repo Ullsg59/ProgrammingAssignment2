8a550bb27fc2b1f0a1d5cf3e5aedd7d7a1f48fab
Ullsg59 <juann_pabloo@hotmail.com>
2021-02-19 21:14:38 -0600

    Update cachematrix.R
    Second Assigment for R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions will store a matrix and cache its inverse
 
-## Write a short comment describing this function
+## Here we will first set and get the value of the matrix, and then of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  imatrix <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
 
+  get <- function() x
+    setInvMat <- function(inverse) imatrix <<- inverse
+    getInvMat <- function(){
+      imatrix
+    }
+  list(set = set,
+       get = get,
+       setInvMat = setInvMat,
+       getInvMat = getInvMat)
+  
 }
 
 
-## Write a short comment describing this function
+## This will calculate the inverse of the matrix returned above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  imatrix <- x$getInvMat()
+  if (!is.null(imatrix)) {
+    message("getting cached data")
+    return(imatrix)
+  }
+  data <- x$get()
+  imatrix <- solve(data, ...)
+  x$setInvMat(imatrix)
+  imatrix
 }
+
